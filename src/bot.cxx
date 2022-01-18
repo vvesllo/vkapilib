2082c4ab2af8066249663a247e30a04846bdd20c
weasel <70818012+weaselllll@users.noreply.github.com>
2022-01-18 11:19:14 +0000

    Update bot.cxx

--- src/bot.cxx
+++ src/bot.cxx
@@ -5,18 +5,16 @@ int main()
 {
     std::locale loc;
     std::string token = "token";
-    vkapilib::VKBot bot(194720349, token, "5.131", "ru");
+    vkapilib::VKBot bot(-1, token, "5.131", "ru");
     for(;;)
     {
         bot.update();
         if (bot.getMessage() == u8"пинг") {
-            std::string params[][2] = {
+            bot.call("messages.send", std::map<std::string, std::string> {
                 {"peer_id", std::to_string(bot.peerId())},
                 {"message", "понг"},
-                {"random_id", "0"},
-                {"access_token", token}
-            };
-            bot.call("messages.send", params, sizeof(params)/sizeof(params[0]));
+                {"random_id", "0"}
+            });
         }
     }
 }
